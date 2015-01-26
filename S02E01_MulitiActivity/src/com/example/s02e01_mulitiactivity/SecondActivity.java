5be12b4f519c50ce98528f6b7685ed58c7f1ba3f
roytest001 <xxjbs001@gmail.com>
2015-01-27 00:04:48 +0800

    S01_E01
    S01_E01

---
+++ S02E01_MulitiActivity/src/com/example/s02e01_mulitiactivity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.s02e01_mulitiactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+
+}
